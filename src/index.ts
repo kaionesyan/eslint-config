b84c336b9174cfa20d75c4e1c94e0ea92e351211
Kaio <kaionesyan@gmail.com>
2024-01-10 14:07:52 -0300

    chore: bump version to 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,20 @@ const config: Linter.Config = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['import-helpers'],
+  rules: {
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', ['parent', 'sibling', 'index']],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true,
+        },
+      },
+    ],
+  },
 };
 
 export = config;
